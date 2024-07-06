c78b5859f19968cba749b2cabea9ffd5ab74b6ab
Makalister <makalisterhonorato@gmail.com>
2024-07-06 20:20:20 -0300

    enviando arquivos

---
+++ TiposBasicosStrings/TiposBasicosStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿int telefone = 12378;
+
+long cpf = 62981530099;
+
+float salario = 1000.0f;
+
+double volume = 12.3;
+
+char sexo = 'm';
+
+bool verdadeiroFalso = true;
+
+
+
+Console.WriteLine("Telefone: {0}, CPF: {1}, Salário: {2}, Volume: {3}, Sexo: {4}, Verdadeiro ou Falso: {5}",
+telefone, cpf, salario, volume, sexo, verdadeiroFalso);
+
+
+
+
+Console.WriteLine("Telefone: " + telefone + ", CPF: " + cpf + ", Salário: " + salario + ", Volume: " + volume + "," +
+" Sexo: " + sexo + ", Verdadeiro ou Falso: " + verdadeiroFalso);
