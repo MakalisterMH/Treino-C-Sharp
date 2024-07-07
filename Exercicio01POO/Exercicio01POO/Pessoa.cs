2a6be793bd435b6cd844518e8c9adb3b33af1525
Makalister <makalisterhonorato@gmail.com>
2024-07-07 18:34:20 -0300

    Exercicio01POO

---
+++ Exercicio01POO/Exercicio01POO/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01POO
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+    }
+}
---
+++ Exercicio01POO/Exercicio01POO/Program.cs
@@ -0,0 +1,44 @@
+﻿using Exercicio01POO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int qtdPessoa;
+
+
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.idade = 0;
+
+        Console.WriteLine("Digite quantas pessoa sera digitada: ");
+        qtdPessoa = int.Parse(Console.ReadLine());
+
+
+        for (int i = 1; i <= qtdPessoa; i++)
+        {
+
+            string nome;
+            int idade;
+
+
+
+            Console.WriteLine("Digite o nome da pessoa: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a idade da pessoa: ");
+            idade = int.Parse(Console.ReadLine());
+
+
+            if (pessoa.idade < idade)
+            {
+                pessoa.nome = nome;
+                pessoa.idade = idade;
+            }
+        }
+
+        Console.WriteLine("Pessoa mais velha é: "+ pessoa.nome + " " + pessoa.idade);
+
+    }
+}
