914e4431c2d54a350d1e1ea0807ebe21400a13e5
Makalister <makalisterhonorato@gmail.com>
2024-07-07 05:00:42 -0300

    Enviando Exercicio For

---
+++ ExercicioFor/ExercicioFor/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Quantos numeros vc vai digitar ? ");
+
+int numeroUsuario = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= numeroUsuario; i++)
+{
+    Console.WriteLine("Digite o numero {0}:",i);
+
+    int numero = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Numero {0} = {1}",i,numero);
+}
