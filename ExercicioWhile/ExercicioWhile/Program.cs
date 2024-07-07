80a1d8344f83e4209b0d6100f9685d7e8465fd45
Makalister <makalisterhonorato@gmail.com>
2024-07-07 04:34:21 -0300

    Enviando exercicio while

---
+++ ExercicioWhile/ExercicioWhile/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.WriteLine("Digite um numero: ");
+
+int numeroDigitado = int.Parse(Console.ReadLine());
+
+
+while (numeroDigitado <= 50)
+{
+    Console.WriteLine(Math.Sqrt(numeroDigitado));
+
+    Console.WriteLine("Digite outro numero: ");
+
+    numeroDigitado = int.Parse(Console.ReadLine());
+
+}
+
+Console.WriteLine("Numero maior que 50.");
+
