0e72293507abbcdc22b5f0d9b6ae362c1a2377f5
Makalister <makalisterhonorato@gmail.com>
2024-07-07 03:56:43 -0300

    Enviando Funções e Sintaxe, classe Program e NumeroMaior

---
+++ FuncoesSintaxe/FuncoesSintaxe/NumeroMaior.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncoesSintaxe
+{
+    internal class NumeroMaior
+    {
+
+        int n1, n2 ,n3;
+
+        public int Maior(int n1, int n2, int n3)
+        {
+            int resultado;
+
+            if (n1 > n2 & n1 > n3)
+            {
+                resultado = n1;
+            }
+
+            else if (n2 > n3)
+            {
+                resultado = n2;   // se n1 não é maior é so verificar c n3 é maior q n2, c n for o n2 é o Maior.
+            }
+
+            else
+            {
+                resultado = n3;
+            }
+
+            return resultado;
+
+        }
+
+    }
+}
---
+++ FuncoesSintaxe/FuncoesSintaxe/Program.cs
@@ -0,0 +1,23 @@
+﻿using FuncoesSintaxe;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Digite 3 numeros: ");
+
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int n3 = int.Parse(Console.ReadLine());
+
+        NumeroMaior numeroMaior = new NumeroMaior();
+
+
+        double resultado = numeroMaior.Maior(n1, n2, n3);
+
+        Console.WriteLine("numero maior utilizando a funcao é: {0}." ,resultado);
+
+    }
+
+}
