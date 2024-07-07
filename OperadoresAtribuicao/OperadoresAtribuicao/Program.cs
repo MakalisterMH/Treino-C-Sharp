71a0868064593d5fca582d6e80c76e3be2ecd0e2
Makalister <makalisterhonorato@gmail.com>
2024-07-06 21:32:52 -0300

    Enviando OperadoresAtribuição

---
+++ OperadoresAtribuicao/OperadoresAtribuicao/Program.cs
@@ -0,0 +1,37 @@
+﻿// Operadores de Atribuição
+
+int a = 10;
+Console.WriteLine(a); 
+
+a += 5;
+Console.WriteLine(a); // a = a + 5;
+
+a -= 3;
+Console.WriteLine(a); // a = a - 3;
+
+a *= 2;
+Console.WriteLine(a); // a = a * 2;
+
+
+// Serve tbm para Strings ...
+
+string frase = "Olá";
+
+frase += " Mundo !";
+
+Console.WriteLine(frase);
+
+
+//Operadores atitiméticos / atribuição
+
+// ++    incrementar    a++  =  a + 1;
+// --    decrementar    a--  =  a - 1;
+
+int b = 1;
+b++;
+Console.WriteLine(b);
+
+
+b--;
+Console.WriteLine(b);
+
