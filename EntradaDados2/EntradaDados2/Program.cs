d1ca0e1ee040c8e39be6a41d896d5b17954344d5
Makalister <makalisterhonorato@gmail.com>
2024-07-07 01:27:17 -0300

    Enviando EntradaDados2

---
+++ EntradaDados2/EntradaDados2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+int n1 = int.Parse(Console.ReadLine());
+char sexo = char.Parse(Console.ReadLine());
+double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);    // importancia desse CultureInfo é para ele aceitar entradas flutuantes com " . "
+
+
+Console.WriteLine("Você digitou: {0}",n1);
+Console.WriteLine("Você digitou: {0}", sexo);
+Console.WriteLine("Você digitou: {0}", n2);
+
+
+// Console.WriteLine("Você digitou: {0}", n2.ToString(CultureInfo.InvariantCulture));       c eu colocar só na formatação ele so imprime com o " . " em vez da " , "
+
+
+
+string nome = Console.ReadLine();
+char sx = char.Parse(Console.ReadLine());
+int idade = int.Parse(Console.ReadLine());
+double altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+
+Console.WriteLine("Olá {0} , seu sexo é {1} e você possui a idade {2} anos com {3} de altura",nome,sx,idade,altura);
+
+
