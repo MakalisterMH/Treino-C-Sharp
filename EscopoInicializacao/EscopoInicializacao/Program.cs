69e4706c177580e7d50034c310023dbdc21530c5
Makalister <makalisterhonorato@gmail.com>
2024-07-07 03:18:19 -0300

    Escopo e inicialização

---
+++ EscopoInicializacao/EscopoInicializacao/Program.cs
@@ -0,0 +1,14 @@
+﻿// qualquer variavel tem q ser inicializada para você poder utilizar.       
+
+// declaração de variaveis dentro de uma estrutura so pertence ao escopo daquela estrutura.
+
+
+double preco = double.Parse(Console.ReadLine());
+double desconto = 0.0;                           // inicialização da variavel
+
+if (preco > 100.0)
+{
+    desconto = preco * 0.1;                     // c a variavel tivesse declarada dentro do escopo do if eu n  poderia utilizala para imprimir ela fora.
+}
+
+Console.WriteLine(desconto);
