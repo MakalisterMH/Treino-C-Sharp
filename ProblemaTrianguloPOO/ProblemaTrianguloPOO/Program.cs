3509cf3667fd157dcc474200ad130d63d0a4c235
Makalister <makalisterhonorato@gmail.com>
2024-07-07 14:29:13 -0300

    Problema do triangulo com POO

---
+++ ProblemaTrianguloPOO/ProblemaTrianguloPOO/Program.cs
@@ -0,0 +1,33 @@
+﻿using ProblemaTrianguloPOO;
+using System;
+
+class Program
+{
+    static  void Main(string[] args)
+    {
+
+        double a, b, c;
+
+        Console.WriteLine("Olá vamos calcular o perimetro e a area de um triangulo.");
+
+        Console.WriteLine("Por favor insira o valor do lado A:");
+        a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Por favor insira o valor do lado B:");
+        b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Por favor insira o valor do lado C:");
+        c = double.Parse(Console.ReadLine());
+
+
+        Triangulo triangulo = new Triangulo(a,b,c);
+
+
+
+        Console.WriteLine(triangulo.CalcularPerimetro());
+        Console.WriteLine(triangulo.CalcularArea());
+
+
+    }
+
+}
---
+++ ProblemaTrianguloPOO/ProblemaTrianguloPOO/Triangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemaTrianguloPOO
+{
+    internal class Triangulo
+    {
+
+        double a, b, c, p, area;
+
+        public Triangulo(double a,double b,double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double CalcularPerimetro()
+        {
+            return p = (a + b + c) / 2;
+
+        }
+
+        public double CalcularArea()
+        {
+            return area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+    }
+}
