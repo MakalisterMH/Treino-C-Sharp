eeb35e6b6f7ba3a443b328d7295a1d59a780e43c
Makalister <makalisterhonorato@gmail.com>
2024-07-07 01:02:35 -0300

    Enviando Split Vetor

---
+++ SplitVetor/SplitVetor/Program.cs
@@ -0,0 +1,21 @@
+﻿string s = Console.ReadLine();
+
+string[] vetor = s.Split(' ');
+
+
+// ou =       string[] vetor =  Console.ReadLine().Split(' ');
+
+
+// impressao com for
+
+for (int i = 0; i < vetor.Length; i++)
+{
+    Console.WriteLine(vetor[i]);
+}
+
+// impressao com foreach
+
+foreach (string fruta in vetor)
+{
+    Console.WriteLine(fruta);
+}
