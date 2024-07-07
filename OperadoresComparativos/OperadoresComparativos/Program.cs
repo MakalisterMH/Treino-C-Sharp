16d7c22fef5ddc5a386608581fdb94d1bc3994db
Makalister <makalisterhonorato@gmail.com>
2024-07-07 02:20:28 -0300

    OperadoresComparativos

---
+++ OperadoresComparativos/OperadoresComparativos/Program.cs
@@ -0,0 +1,32 @@
+﻿/*    > ( Maior)     < (menor)      >= (maior ou igual)      <= (menor ou igual)     == (igual)     != (diferente)     */
+
+
+
+int a = 10;
+bool c1 = a < 10;
+
+Console.WriteLine(c1);
+
+
+bool c2 = a < 20;
+Console.WriteLine(c2);
+
+bool c3 = a > 10;
+Console.WriteLine(c3);
+
+
+bool c4 = a > 5;
+Console.WriteLine(c4);
+
+
+bool c5 = a <= 10;
+Console.WriteLine(c5);
+
+bool c6 = a >= 10;
+System.Console.WriteLine(c6);
+
+bool c7 = a == 10;
+Console.WriteLine(c7);
+
+bool c8 = a != 10;
+Console.WriteLine(c8);
