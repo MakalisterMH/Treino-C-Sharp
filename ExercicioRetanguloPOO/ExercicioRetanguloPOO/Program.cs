44ecdb7a8373936c4a61facb347c74169f174ecd
Makalister <makalisterhonorato@gmail.com>
2024-07-08 20:09:04 -0300

    Enviando exercicio POO do retangulo

---
+++ ExercicioRetanguloPOO/ExercicioRetanguloPOO/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExercicioRetanguloPOO;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Retangulo retangulo = new Retangulo();
+
+        Console.WriteLine("Digite a Lagura do retangulo: ");
+        retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+        Console.WriteLine("Digite a Altura do retangulo: ");
+        retangulo.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+        double area,perimetro,diagonal;
+
+        area = retangulo.CalcularArea();
+        perimetro = retangulo.CalcularPerimetro();
+        diagonal = retangulo.CalcularDiagonal();
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine("Area do retangulo: {0}",area.ToString("F2", CultureInfo.InvariantCulture));
+
+        Console.WriteLine("Perimetro do retangulo: {0}", perimetro.ToString("F2", CultureInfo.InvariantCulture));
+
+        Console.WriteLine("Diagonal do retangulo: {0}", diagonal.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+}
---
+++ ExercicioRetanguloPOO/ExercicioRetanguloPOO/Retangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioRetanguloPOO
+{
+    public class Retangulo
+    {
+
+        public double Largura;
+        public double Altura;
+
+
+        public double CalcularArea()
+        {
+        return Largura * Altura; 
+        }
+
+        public double CalcularPerimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+
+        public double CalcularDiagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+        }
+
+    }
+
+}
+
