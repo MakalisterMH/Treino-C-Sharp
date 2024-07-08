8a2b8af3d5816e517a20979a4a578c88fba1a4f2
Makalister <makalisterhonorato@gmail.com>
2024-07-07 21:41:17 -0300

    Enviando problema da orientação objeto com laços de repetição

---
+++ ProblemaProdutoPOO/ProblemaProdutoPOO/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemaProdutoPOO
+{
+    internal class Produto
+    {
+
+        public string nome;
+        public double preco;
+        public int quantidade;
+
+
+
+        public double ValorTotalEmEstoque()
+        {
+
+            return quantidade * preco;
+
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            this.quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            this.quantidade -= quantidade;
+        }
+
+
+    }
+
+    }
+
---
+++ ProblemaProdutoPOO/ProblemaProdutoPOO/Program.cs
@@ -0,0 +1,61 @@
+﻿using ProblemaProdutoPOO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numeroUsuario = -1;
+
+        Produto produto = new Produto();
+
+        while (numeroUsuario != 0)
+        {
+
+
+            Console.WriteLine("Digite 1 para adicionar produtos ou 2 para remover, se desejar sair do programa digite 0.");
+            numeroUsuario = int.Parse(Console.ReadLine());
+
+       
+
+        if (numeroUsuario == 1)
+        {
+            Console.WriteLine("Digite o nome do produto: ");
+            produto.nome = Console.ReadLine();
+
+
+            Console.WriteLine("Digite o preço do produto: ");
+            produto.preco = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite a quantidade do produto: ");
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+
+
+
+
+        }
+
+
+            if (numeroUsuario == 2)
+            {
+                Console.WriteLine("Digite o nome do produto: ");
+                produto.nome = Console.ReadLine();
+
+
+                Console.WriteLine("Digite o preço do produto: ");
+                produto.preco = double.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Digite a quantidade do produto: ");
+                produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+
+
+
+            }
+
+            Console.WriteLine("Valor total no estoque é de: {0}", produto.ValorTotalEmEstoque());
+
+        }
+
+    }
+}
