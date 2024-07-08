cdf9889d615e1b7d42279a9b68838c9283e0a839
Makalister <makalisterhonorato@gmail.com>
2024-07-08 20:31:48 -0300

    Enviando exercicio POO do Funcionario

---
+++ ExercicioFuncionarioPOO/ExercicioFuncionarioPOO/Funcionario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFuncionarioPOO
+{
+    internal class Funcionario
+    {
+        public string nome;
+        public double salario;
+        public double imposto;
+
+        public double SalarioLiquido()
+        {
+            return salario = imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            salario += (porcentagem * salario) / 100; 
+        }
+
+    }
+}
---
+++ ExercicioFuncionarioPOO/ExercicioFuncionarioPOO/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExercicioFuncionarioPOO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Funcionario funcionario = new Funcionario();
+
+        Console.WriteLine("Digite o nome do funcionario");
+        funcionario.nome = Console.ReadLine();
+        string nome = funcionario.nome;
+
+
+        Console.WriteLine("Digite o salario do funcionario");
+        funcionario.salario = double.Parse(Console.ReadLine());
+        double salario = funcionario.salario;
+
+        Console.WriteLine("Digite a porcentagem de aumento do salario desse funcionario");
+        double porcentagem = double.Parse(Console.ReadLine());
+
+        funcionario.AumentarSalario(porcentagem);
+
+        Console.WriteLine("Considerando o aumento de {0}% sobre o salario de {1} $, o salario atualizado fica: {2} $.",porcentagem,salario,funcionario.salario);
+    }
+}
