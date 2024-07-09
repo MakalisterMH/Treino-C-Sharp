a0890c39550d3187a5d10229b10b4cce7d45e3ed
Makalister <makalisterhonorato@gmail.com>
2024-07-09 00:16:34 -0300

    Membros estaticos 3

---
+++ ExercicioConversorMoeda/ExercicioConversorMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioConversorMoeda
+{
+    internal class ConversorDeMoeda
+    {
+
+        double cotacao;
+        double quantidade;
+
+
+        public static double CotacaoDolar(double cotacao,double quantidade)
+        {
+            double resultado = cotacao * quantidade;
+            return resultado += resultado * 6 / 100;
+        }
+
+
+
+    }
+}
---
+++ ExercicioConversorMoeda/ExercicioConversorMoeda/Program.cs
@@ -0,0 +1,26 @@
+﻿using ExercicioConversorMoeda;
+
+class Program{
+    static void Main(string[] args)
+    {
+
+
+        
+
+        Console.WriteLine("Qual é a cotação do dolar ? ");
+        double cotacao = double.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("Qual é a quantidade de dolares que você quer comprar ? ");
+        double quantidade = double.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("Valor a ser pago em reais: "+ ConversorDeMoeda.CotacaoDolar(cotacao, quantidade).ToString("F2"));
+
+        
+
+
+    }
+}
+
+
