794640f4fcf23dcfd3231d068dd0af9014d2e567
Makalister <makalisterhonorato@gmail.com>
2024-07-09 21:39:43 -0300

    Encapsulamento

---
+++ Encapsulamento/Encapsulamento/Pessoa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+
+
+        public Pessoa(string nome,int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public string GetNome()
+        {
+        return this.nome; 
+        }
+
+        public int GetIdade()
+        {
+         return this.idade;
+        }
+
+
+        public void SetNome(int idade)
+        {
+            this.idade = idade;
+        }
+
+        public void SetIdade(int idade)
+        {
+            this.idade = idade;
+        }
+
+
+
+
+
+
+
+
+
+        public override string ToString()
+        {
+            return "Nome: "+ GetNome() + " Idade: " + GetIdade();
+        }
+    }
+}
---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,14 @@
+﻿using Encapsulamento;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Pessoa pessoa = new Pessoa("Makalister",24);
+
+
+        Console.WriteLine(pessoa.ToString());
+
+    }
+}
