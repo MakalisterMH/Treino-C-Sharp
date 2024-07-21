b8cdc7bda93e5a9cdb918de24da1d6be7bb51bb9
Makalister Honorato <makalisterhonorato@gmail.com>
2024-07-21 19:35:47 -0300

    conflito 2

--- Listas/Listas/Program.cs
+++ Listas/Listas/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Aqui vamos fazer as listas  1 arrumado !");
+Console.WriteLine("Aqui vamos fazer as listas criando conflito 2 !");
 
