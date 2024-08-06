7887514c2130ac22e65e56658d0555afcf440fa9
Makalister <makalisterhonorato@gmail.com>
2024-08-06 01:42:47 -0300

    Projeto para baixar TREs

---
+++ SeleniumAutomacao/SeleniumAutomacao/Program.cs
@@ -0,0 +1,178 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using WindowsInput;
+using WindowsInput.Native;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+
+namespace SeleniumAutomation
+{
+    class Program
+    {
+        [STAThread] // Necessário para enviar teclas
+        static void Main(string[] args)
+        {
+
+
+
+
+            // Diretório de origem
+            string sourceDirectory = @"C:\Users\LocalAdmin\Downloads";
+
+            // Diretório de destino
+            string destinationDirectory = @"C:\Users\LocalAdmin\Desktop\auxiliar";
+
+            // Verificar se o diretório de origem existe
+            if (!Directory.Exists(sourceDirectory))
+            {
+                Console.WriteLine($"O diretório de origem '{sourceDirectory}' não existe.");
+                return;
+            }
+
+            // Criar o diretório de destino se não existir
+            if (!Directory.Exists(destinationDirectory))
+            {
+                Directory.CreateDirectory(destinationDirectory);
+            }
+
+            // Obter todos os arquivos do diretório de origem
+            string[] files = Directory.GetFiles(sourceDirectory);
+
+            foreach (var filePath in files)
+            {
+                // Obter o nome do arquivo
+                string fileName = Path.GetFileName(filePath);
+
+                // Caminho de destino para o arquivo
+                string destFilePath = Path.Combine(destinationDirectory, fileName);
+
+                try
+                {
+                    // Mover o arquivo
+                    File.Move(filePath, destFilePath);
+                    Console.WriteLine($"Arquivo movido: {fileName}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao mover o arquivo {fileName}: {ex.Message}");
+                }
+            }
+
+            Console.WriteLine("Todos os arquivos foram movidos com sucesso!");
+
+
+
+            Console.WriteLine("Agora vamos baixar os jornais");
+
+            
+
+
+
+
+
+            // Obter a data e hora atual
+            DateTime agora = DateTime.Now;
+
+            // Formatando a data no formato dd/MM/yyyy
+            string dataOriginal = agora.ToString("dd/MM/yyyy");
+
+            // Substituir "/" por "-"
+            string dataFormatada = dataOriginal.Replace("/", "-");
+
+
+
+
+            // Configurar o caminho para o GeckoDriver
+            string geckoDriverPath = @"C:\WebDriver";
+
+            // Inicializar o FirefoxDriver
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(geckoDriverPath);
+            IWebDriver driver = new FirefoxDriver(service);
+
+            // Navegar para a página
+            driver.Navigate().GoToUrl("https://dje-consulta.tse.jus.br/#/dje/calendario");
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            for (int i = 2; i < 29; i++)
+            {
+                // Esperar até que o primeiro elemento esteja clicável e clicar nele
+                IWebElement element = wait.Until(drv => drv.FindElement(By.XPath("/html/body/app-root/div/app-calendario/div/div[1]/div[1]/div/div[1]/mat-form-field/div/div[1]/div")));
+                element.Click();
+
+                // Esperar até que o segundo elemento esteja clicável e clicar nele
+                string xpathSecondElement = $"/html/body/div[4]/div[2]/div/div/div/mat-option[{i}]/span";
+                IWebElement secondElement = wait.Until(drv => drv.FindElement(By.XPath(xpathSecondElement)));
+                secondElement.Click();
+
+                for (int y = 2; y < 6; y++)
+                {
+                    try
+                    {
+                        Thread.Sleep(1000);
+
+                        // Esperar até que o terceiro elemento esteja clicável e clicar nele
+                        string xpathTerceiroElement = $"/html/body/app-root/div/app-calendario/div/div[1]/div[2]/div[{y}]/button/span/mat-icon";
+                        IWebElement terceiroElement = wait.Until(drv => drv.FindElement(By.XPath(xpathTerceiroElement)));
+                        terceiroElement.Click();
+
+                        // Esperar até que a nova janela esteja disponível
+                        wait.Until(drv => drv.WindowHandles.Count > 1);
+
+                        // Obter a janela atual e a nova janela
+                        string mainWindowHandle = driver.CurrentWindowHandle;
+                        string newWindowHandle = driver.WindowHandles.First(handle => handle != mainWindowHandle);
+
+                        // Alternar para a nova janela
+                        driver.SwitchTo().Window(newWindowHandle);
+
+                        Thread.Sleep(1000);
+
+                        // Esperar até que o quarto elemento esteja clicável e clicar nele
+                        string xpathQuartoElement = "//*[@id='download']";
+                        IWebElement quartoElement = wait.Until(drv => drv.FindElement(By.XPath(xpathQuartoElement)));
+                        quartoElement.Click();
+
+                        // Esperar até que a janela de diálogo esteja ativa
+                        Thread.Sleep(6000); // Aguarde um pouco para garantir que a janela esteja ativa
+
+                        // Usar InputSimulator para enviar texto
+                        var simulator = new InputSimulator();
+                        simulator.Keyboard.TextEntry($"jornal{i}-{y}-{dataFormatada}.pdf");
+
+                        Thread.Sleep(1000); // Aguarde um pouco para garantir que a janela esteja ativa
+
+                        simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN); // Pressiona Enter para salvar
+
+                        Thread.Sleep(2000);
+
+                        // Fechar a nova janela e voltar para a janela principal
+                        driver.Close();
+                        driver.SwitchTo().Window(mainWindowHandle);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Lidar com exceções
+                        Console.WriteLine($"Erro ao processar o elemento {y}: {ex.Message}");
+                        // Se desejar continuar com o próximo y, adicione um continue aqui, se apropriado
+                    }
+                }
+            }
+
+            // Fechar o navegador
+            driver.Quit();
+
+
+
+
+
+    }
+    }
+}
---
+++ VerificarRenomear/VerificarRenomear/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+
+namespace PDFTextExtraction
+{
+    class Program
+    {
+        // Dicionário que mapeia os nomes completos dos tribunais para suas siglas
+        private static readonly Dictionary<string, string> TribunalAbbreviations = new Dictionary<string, string>
+        {
+            { "TRIBUNAL REGIONAL ELEITORAL DO ACRE", "TRE-AC" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE ALAGOAS", "TRE-AL" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO AMAZONAS", "TRE-AM" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO AMAPÁ", "TRE-AP" },
+            { "TRIBUNAL REGIONAL ELEITORAL DA BAHIA", "TRE-BA" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO CEARÁ", "TRE-CE" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO DISTRITO", "TRE-DF" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO ESPÍRITO SANTO", "TRE-ES" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE GOIÁS", "TRE-GO" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO MARANHÃO", "TRE-MA" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO MATO GROSSO", "TRE-MT" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE MATO GROSSO DO SUL", "TRE-MS" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE MINAS GERAIS", "TRE-MG" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO PARÁ", "TRE-PA" },
+            { "TRIBUNAL REGIONAL ELEITORAL DA PARAÍBA", "TRE-PB" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO PARANÁ", "TRE-PR" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE PERNAMBUCO", "TRE-PE" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO PIAUÍ", "TRE-PI" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO RIO DE JANEIRO", "TRE-RJ" },
+            { "NORTE", "TRE-RN" },
+            { "SUL", "TRE-RS" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE RONDÔNIA", "TRE-RO" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE RORAIMA", "TRE-RR" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE SANTA CATARINA", "TRE-SC" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE SÃO PAULO", "TRE-SP" },
+            { "TRIBUNAL REGIONAL ELEITORAL DE SERGIPE", "TRE-SE" },
+            { "TRIBUNAL REGIONAL ELEITORAL DO TOCANTINS", "TRE-TO" }
+        };
+
+        // Dicionário que mapeia os nomes dos meses para seus números correspondentes
+        private static readonly Dictionary<string, string> MonthToNumeric = new Dictionary<string, string>
+        {
+            { "janeiro", "01" },
+            { "fevereiro", "02" },
+            { "março", "03" },
+            { "abril", "04" },
+            { "maio", "05" },
+            { "junho", "06" },
+            { "julho", "07" },
+            { "agosto", "08" },
+            { "setembro", "09" },
+            { "outubro", "10" },
+            { "novembro", "11" },
+            { "dezembro", "12" }
+        };
+
+        static void Main(string[] args)
+        {
+            // Diretório onde os arquivos PDF estão localizados
+            string sourceDirectory = @"C:\Users\LocalAdmin\Downloads";
+
+            // Verifica se o diretório especificado existe
+            if (!Directory.Exists(sourceDirectory))
+            {
+                Console.WriteLine($"O diretório '{sourceDirectory}' não existe.");
+                return;
+            }
+
+            // Obtém todos os arquivos PDF no diretório
+            string[] files = Directory.GetFiles(sourceDirectory, "*.pdf");
+
+            // Itera sobre cada arquivo PDF encontrado
+            foreach (string file in files)
+            {
+                try
+                {
+                    // Variáveis para armazenar dados extraídos do PDF
+                    string edicao = string.Empty;
+                    string disponibilizacaoDay = string.Empty;
+                    string publicacaoDay = string.Empty;
+                    string tribunalAbbreviation = string.Empty;
+                    string mesDis = string.Empty;
+                    string mesPub = string.Empty;
+                    string anoDis = string.Empty;
+                    string anoPub = string.Empty;
+
+                    // Abre o arquivo PDF para leitura
+                    using (PdfReader pdfReader = new PdfReader(file))
+                    {
+                        using (PdfDocument pdfDoc = new PdfDocument(pdfReader))
+                        {
+                            // Verifica se o PDF contém pelo menos uma página
+                            if (pdfDoc.GetNumberOfPages() > 0)
+                            {
+                                // Obtém a primeira página do PDF
+                                var page = pdfDoc.GetPage(1);
+                                // Extrai o texto da primeira página
+                                string text = ExtractTextFromPage(page);
+
+                                // Verifica o tipo de edição e extrai o valor da edição
+                                edicao = ExtractEdition(text);
+
+                                // Extrai o dia de disponibilização e publicação
+                                disponibilizacaoDay = ExtractDay(text, "Disponibilização:");
+                                publicacaoDay = ExtractDay(text, "Publicação:");
+
+                                // Obtém a sigla do tribunal
+                                tribunalAbbreviation = GetTribunalAbbreviation(text);
+
+                                // Extrai o mês e ano de disponibilização e publicação
+                                mesDis = ExtractMonth(text, "Disponibilização:");
+                                mesPub = ExtractMonth(text, "Publicação:");
+                                anoDis = ExtractYear(text, "Disponibilização:");
+                                anoPub = ExtractYear(text, "Publicação:");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"O arquivo {Path.GetFileName(file)} está vazio.");
+                                continue;
+                            }
+                        }
+                    }
+
+                    // Obtém o mês numérico a partir do nome do mês
+                    string mesDisNumerico = MonthToNumeric.ContainsKey(mesDis.ToLower()) ? MonthToNumeric[mesDis.ToLower()] : "00";
+                    string mesPubNumerico = MonthToNumeric.ContainsKey(mesPub.ToLower()) ? MonthToNumeric[mesPub.ToLower()] : "00";
+
+                    // Cria o novo nome do arquivo no formato desejado
+                    string novoNome = $"{tribunalAbbreviation}_{anoPub}{mesPubNumerico}{publicacaoDay}_{edicao}_Judiciário_{anoDis}{mesDisNumerico}{disponibilizacaoDay}0000.pdf";
+
+                    // Define o caminho completo para o novo nome do arquivo
+                    string novoCaminho = Path.Combine(sourceDirectory, novoNome);
+                    // Renomeia o arquivo
+                    File.Move(file, novoCaminho);
+
+                    // Exibe informações sobre o arquivo processado
+                    Console.WriteLine($"Arquivo: {Path.GetFileName(file)}");
+                    Console.WriteLine($"Novo Nome: {novoNome}");
+                    Console.WriteLine(new string('-', 80));
+                }
+                catch (Exception ex)
+                {
+                    // Exibe erros que ocorrem durante o processamento
+                    Console.WriteLine($"Erro ao processar o arquivo {Path.GetFileName(file)}: {ex.Message}");
+                }
+            }
+
+            // Mensagem final indicando que o processamento foi concluído
+            Console.WriteLine("Processamento concluído.");
+        }
+
+        // Método para extrair o texto de uma página do PDF
+        private static string ExtractTextFromPage(iText.Kernel.Pdf.PdfPage page)
+        {
+            // Define a estratégia de extração de texto
+            var strategy = new SimpleTextExtractionStrategy();
+            // Extrai o texto da página usando a estratégia definida
+            var text = PdfTextExtractor.GetTextFromPage(page, strategy);
+            return text;
+        }
+
+        // Método para extrair a edição do texto
+        private static string ExtractEdition(string text)
+        {
+            // Verifica se o texto contém "Edição Extraordinária"
+            if (text.Contains("Edição Extraordinária"))
+            {
+                // Encontra a posição de "nº" e "Edição Extraordinária" no texto
+                var indiceN = text.IndexOf("nº");
+                var indiceDis = text.IndexOf("Edição Extraordinária", indiceN);
+                // Extrai a edição entre essas posições
+                if (indiceN != -1 && indiceDis != -1)
+                {
+                    var edicao = text.Substring(indiceN + 2, indiceDis - indiceN - 2).Trim();
+                    return Regex.Replace(edicao, @"\s", "");
+                }
+            }
+            // Verifica se o texto contém "Edição Eleitoral"
+            else if (text.Contains("Edição Eleitoral"))
+            {
+                // Encontra a posição de "nº" e "Edição Eleitoral" no texto
+                var indiceN = text.IndexOf("nº");
+                var indiceDis = text.IndexOf("Edição Eleitoral", indiceN);
+                // Extrai a edição entre essas posições
+                if (indiceN != -1 && indiceDis != -1)
+                {
+                    var edicao = text.Substring(indiceN + 2, indiceDis - indiceN - 2).Trim();
+                    return Regex.Replace(edicao, @"\s", "");
+                }
+            }
+            else
+            {
+                // Para outros casos, tenta extrair o valor da edição
+                var indiceN = text.IndexOf("nº");
+                var indiceDis = text.IndexOf("Disponibilização", indiceN);
+                if (indiceN != -1 && indiceDis != -1)
+                {
+                    var edicao = text.Substring(indiceN + 2, indiceDis - indiceN - 2).Trim();
+                    return Regex.Replace(edicao, @"\s", "");
+                }
+            }
+
+            return string.Empty;
+        }
+
+        // Método para extrair o dia de uma data a partir do texto
+        private static string ExtractDay(string text, string context)
+        {
+            // Define o padrão para extrair o dia
+            var dayPattern = $@"{context}.*?(\d{{2}}) de \w+ de \d{{4}}";
+            var match = Regex.Match(text, dayPattern, RegexOptions.IgnoreCase);
+            return match.Success ? match.Groups[1].Value : string.Empty;
+        }
+
+        // Método para extrair o mês de uma data a partir do texto
+        private static string ExtractMonth(string text, string context)
+        {
+            // Define o padrão para extrair o mês
+            var monthPattern = $@"{context}.*?de (\w+) de \d{{4}}";
+            var match = Regex.Match(text, monthPattern, RegexOptions.IgnoreCase);
+            return match.Success ? match.Groups[1].Value : string.Empty;
+        }
+
+        // Método para extrair o ano de uma data a partir do texto
+        private static string ExtractYear(string text, string context)
+        {
+            // Define o padrão para extrair o ano
+            var yearPattern = $@"{context}.*?de \w+ de (\d{{4}})";
+            var match = Regex.Match(text, yearPattern, RegexOptions.IgnoreCase);
+            return match.Success ? match.Groups[1].Value : string.Empty;
+        }
+
+        // Método para obter a sigla do tribunal a partir do texto
+        private static string GetTribunalAbbreviation(string text)
+        {
+            // Itera sobre cada par chave-valor do dicionário de siglas
+            foreach (var pair in TribunalAbbreviations)
+            {
+                // Verifica se o texto contém o nome completo do tribunal
+                if (text.Contains(pair.Key))
+                {
+                    // Retorna a sigla correspondente
+                    return pair.Value;
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
