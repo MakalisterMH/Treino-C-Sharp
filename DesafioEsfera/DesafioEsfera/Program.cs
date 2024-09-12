54e623dd082d87ea00723a0272040c84d29b3974
Makalister <makalisterhonorato@gmail.com>
2024-09-12 20:28:30 -0300

    Enviando desafio da esfera do beecrowd

---
+++ DesafioEsfera/DesafioEsfera/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+
+using System.Globalization;
+
+double pi = 3.14159;
+
+
+double raio = double.Parse(Console.ReadLine());
+
+
+double resultado = (4/3.0) * pi * Math.Pow(raio, 3);
+
+
+Console.WriteLine("VOLUME = "+ resultado.ToString("F3",CultureInfo.InvariantCulture));
+
+
+
