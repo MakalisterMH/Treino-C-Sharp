b19d993bc6d64fecb80447ce3ac25305f2924822
Makalister <makalisterhonorato@gmail.com>
2025-01-02 22:44:24 -0300

    ListaTreinamento2

---
+++ ListaTreinamento2/ListaTreinamento2/Program.cs
@@ -0,0 +1,69 @@
+﻿using ListaTreinamento2;
+
+class Program
+{
+    static void Main()
+    {
+
+        List<Tarefa> listaTarefas = new List<Tarefa>();
+        
+
+        while (true)
+        {
+
+
+            Console.WriteLine("Digite 1 para inserir tarefa na lista, 2 para deletar tarefa da lista e 3 para imprimir todas as tarefas");
+            int usuarioNumero = int.Parse(Console.ReadLine());
+
+            if (usuarioNumero == 1)
+            {
+                Console.WriteLine("Digite o nome que deseja inserir na tarefa: ");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Digite a descrição que deseja inserir na tarefa: ");
+                string descricao = Console.ReadLine();
+
+                Console.WriteLine("Digite a prioridade que deseja inserir na tarefa: 1 para alta, 2 para media e 3 para baixa.");
+                int prioridade = int.Parse(Console.ReadLine());
+
+                Tarefa tarefa = new Tarefa(nome,descricao,prioridade);
+                listaTarefas.Add(tarefa);
+
+            }
+
+            else if (usuarioNumero == 2)
+            {
+                
+
+                Console.WriteLine("Digite o nome da tarefa que deseja remover na lista: ");
+                string nome = Console.ReadLine();
+
+
+
+                foreach (Tarefa tarefa in listaTarefas)
+                {
+                    if (tarefa.Nome.Equals(nome, StringComparison.OrdinalIgnoreCase))
+                    {
+                        listaTarefas.Remove(tarefa);
+
+                        break; // Encontramos a tarefa e removemos, podemos sair do loop
+                    }
+                }
+
+            }
+
+
+            else if (usuarioNumero == 3)
+            {
+                foreach (Tarefa tarefa in listaTarefas)
+                {
+                    Console.WriteLine(tarefa);
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ ListaTreinamento2/ListaTreinamento2/Tarefa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaTreinamento2
+{
+    internal class Tarefa
+    {
+        string nome;
+        string descricao;
+        int prioridade;
+
+        public Tarefa()
+        {
+        }
+
+        public Tarefa(string nome,string descricao,int prioridade)
+        {
+            this.nome = nome;
+            this.descricao = descricao;
+            this.prioridade = prioridade;
+        }
+
+        // Propriedade Nome
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        // Propriedade Descricao
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+        // Propriedade Prioridade
+        public int Prioridade
+        {
+            get { return prioridade; }
+            set { prioridade = value; }
+        }
+
+
+        public override string ToString()
+        {
+            return $"Nome: {nome}, Descrição: {descricao}, Prioridade: {prioridade}";
+        }
+
+    }
+}
