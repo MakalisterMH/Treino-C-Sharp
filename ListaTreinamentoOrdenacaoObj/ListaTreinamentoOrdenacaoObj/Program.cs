aeba632bb7ae9b102a43924342b3ae37910472ce
Makalister <makalisterhonorato@gmail.com>
2025-01-02 23:30:57 -0300

    ListaTreinamento Ordenacao de obj com e sem lambda

---
+++ ListaTreinamentoOrdenacaoObj/ListaTreinamentoOrdenacaoObj/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ListaTreinamentoOrdenacaoObj { 
+class Program
+    {
+        static void Main()
+        {
+            List<Pessoa> listaPessoas = new List<Pessoa>
+        {
+            new Pessoa("Ana", 25),
+            new Pessoa("João", 32),
+            new Pessoa("Carlos", 18),
+            new Pessoa("Maria", 27)
+        };
+
+            // Ordenando por Idade (crescente)
+            listaPessoas.Sort((x, y) => x.Idade.CompareTo(y.Idade));
+
+            // Exibindo a lista ordenada
+            foreach (var pessoa in listaPessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("sem lambda ========================================================================================");
+
+            Console.WriteLine(" ");
+
+
+
+            // Ordenando a lista usando o comparador
+            listaPessoas.Sort(Compare);
+
+            // Exibindo a lista ordenada
+            foreach (var pessoa in listaPessoas)
+            {
+                Console.WriteLine(pessoa);
+            }
+
+
+            static int Compare(Pessoa x, Pessoa y)
+            {
+                // Comparando as idades
+                return x.Idade.CompareTo(y.Idade);
+            }
+
+
+        }
+    }
+}
+
