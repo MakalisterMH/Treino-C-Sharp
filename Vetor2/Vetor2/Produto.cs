7fe268831ca1b3c8df234d41df7eba0b34b515ed
Makalister <makalisterhonorato@gmail.com>
2025-01-13 22:30:40 -0300

    vetor2 com obj

---
+++ Vetor2/Vetor2/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetor2
+{
+    internal class Produto
+    {
+
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Valor = preco;
+        }
+
+        // Sobrescreve o método ToString para formatar a saída
+        public override string ToString()
+        {
+            return $"Produto: {Nome}, Valor: R${Valor:F2}";
+        }
+
+
+       
+
+
+    }
+
+
+
+}
---
+++ Vetor2/Vetor2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Channels;
+using Vetor2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Digite o tamanho do vetor de produtos: ");
+        int tamanho = int.Parse(Console.ReadLine());
+
+        Produto[] produtos = new Produto[tamanho];
+        
+
+        for (int i = 0; i < produtos.Length; i++)
+        {
+         
+
+            Console.WriteLine("Digite o nome do produto na posição "+i);
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do produto na posição " + i);
+            double valor = double.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(nome,valor);
+
+            produtos[i] = produto;
+        }
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+
+        for (int i = 0;i < produtos.Length; i++)
+        {
+            Console.WriteLine(produtos[i]);
+        }
+
+
+        
+
+
+        double CalculoMedia(Produto[] produtos)
+        {
+            double resultado = 0;
+
+            for (int i = 0; i < produtos.Length; i++)
+            {
+                
+                resultado += produtos[i].Valor;
+                
+            }
+            return resultado / produtos.Length;
+
+        }
+
+        Console.WriteLine("Media: "+ CalculoMedia(produtos));
+
+    }
+
+}
