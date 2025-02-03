6422eafaa4962adc3d6fc8787e759b65aed03714
Makalister <makalisterhonorato@gmail.com>
2025-02-02 22:55:32 -0300

    Boxing e Unboxing

---
+++ BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+        int x = 10;
+
+
+        Object obj = x;   // Processo de fazer um objeto do tipo valor virar referencia ...
+
+        Console.WriteLine(obj);
+
+
+        int y = (int)obj;   // Processo de fazer um objeto do tipo referencia virar um tipo valor compativel...
+
+        Console.WriteLine(y);
+
+    }
+}
